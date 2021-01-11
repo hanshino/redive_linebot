ad37990fae18f49be9cedb821341c3a21d1c7fdb
hanshino <c0952199517@gmail.com>
2021-01-11 23:47:39 +0800

    啟動消息排程

--- job/index.js
+++ job/index.js
@@ -3,7 +3,6 @@ const script = require("./bin");
 
 let monthJob = new CronJob("0 0 0 1 * *", script.Group.resetRecords);
 let dailyJob = new CronJob("0 0 0 * * *", daily);
-let nonStopJob = new CronJob("0 0 * * * *", script.heartbeat);
 let eventRunning = false;
 let eventJob = new CronJob("* * * * * *", async () => {
   if (eventRunning) return;
@@ -25,12 +24,21 @@ let spiderJob = new CronJob("0 1,6,11,16,21,26,31,36,41,46,51,56 * * * *", async
   await script.Spider.main();
 });
 
+let consumeNotifyJob = new CronJob("*/10 * * * * *", async () => {
+  await script.Notify.consumeNotifyList();
+});
+
+let provideNotifyJob = new CronJob("5 * * * * *", async () => {
+  await script.Notify.provideNotifyList();
+});
+
 dailyJob.start();
-nonStopJob.start();
 eventJob.start();
 monthJob.start();
 updateRecordJob.start();
 spiderJob.start();
+provideNotifyJob.start();
+consumeNotifyJob.start();
 
 async function daily() {
   await Promise.all([
