3734b94a75a03cde692519b59900c1a1d282971f
hanshino <hanshino@pchome.tw>
2021-04-05 01:55:25 +0800

    修正階段計算bug

--- app/src/controller/princess/battle.js
+++ app/src/controller/princess/battle.js
@@ -645,7 +645,7 @@ exports.api.updateGuildBattleConfig = async (req, res) => {
  */
 function getStageByWeek(week, version = "TW") {
   let stages = Stages.filter(stage => stage.version.indexOf(version) !== -1);
-  let { stage } = stages.find(stage => stage.min < week && stage.max > week);
+  let { stage } = stages.find(stage => stage.min <= week && stage.max >= week);
 
   return stage;
 }
