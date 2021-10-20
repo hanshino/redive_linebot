63d83fdfbeda89f12133048dd41e915f8b0affb4
cameron.hsieh <cameron.hsieh@coolbe.com.tw>
2021-10-20 18:12:19 +0800

    middleware設置user id

--- app/src/middleware/profile.js
+++ app/src/middleware/profile.js
@@ -1,4 +1,6 @@
 const { getGroupSummary, getGroupCount } = require("../util/line");
+const UserModel = require("../model/application/UserModel");
+const redis = require("../util/redis");
 
 /**
  * 設置用戶、群組資料
@@ -10,7 +12,11 @@ module.exports = async (context, props) => {
     case "line":
       // 不處理無userId的用戶
       if (context.event.source.userId === undefined) return props.next;
-      await Promise.all([setLineProfile(context), setLineGroupSummary(context)]);
+      await Promise.all([
+        setLineProfile(context),
+        setLineGroupSummary(context),
+        setUserId(context),
+      ]);
       break;
     default:
       break;
@@ -50,5 +56,23 @@ async function setLineGroupSummary(context) {
     context.setState({
       groupDatas: { ...summary, ...groupCount },
     });
+  }
+}
+
+async function setUserId(context) {
+  // 優先使用 redis 的 userId
+  const userId = await redis.get(`user:${context.event.source.userId}`);
+
+  if (userId) {
+    context.event._rawEvent.source = { ...context.event.source, id: userId };
+    return;
+  }
+
+  // 如果 redis 沒有 userId，則查詢資料庫
+  const id = await UserModel.getId(context.event.source.userId);
+
+  if (id) {
+    await redis.set(`user:${context.event.source.userId}`, id);
+    context.event._rawEvent.source = { ...context.event.source, id };
   }
 }
