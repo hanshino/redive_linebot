3c4694598bd96c8ea090ec77bf4a6fa9429ebedf
hanshino <c0952199517@gmail.com>
2021-12-14 23:12:44 +0800

    訊息調整

--- app/src/controller/princess/GodStoneShop/router.js
+++ app/src/controller/princess/GodStoneShop/router.js
@@ -4,6 +4,7 @@ const GodStoneShopModel = require("../../../model/princess/GodStoneShop");
 const CharacterModel = require("../../../model/princess/character");
 const GodStoneShopTemplate = require("../../../templates/princess/GodStoneShop");
 const chunk = require("lodash/chunk");
+const i18n = require("../../../util/i18n");
 
 exports.router = [text(/^[.#]轉蛋(兌換|商店)/, showStoneShop)];
 
@@ -31,7 +32,7 @@ async function showStoneShop(context) {
     .filter(item => item);
 
   if (viewData.length === 0) {
-    return context.replyText("沒有任何資料");
+    return context.replyText(i18n.__("message.god_stone_shop_empty"));
   }
 
   const bubbles = viewData.map(item => GodStoneShopTemplate.genShopItem(item));
