1e54681d9cc80d5607f4c01c816775a607166473
hanshino <c0952199517@gmail.com>
2021-12-26 13:30:41 +0800

    CD腳本修正

--- scripts/traefik-run/001_run.sh
+++ scripts/traefik-run/001_run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml build
-
-"${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml exec bot yarn migrate
-
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml up -d
+"${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml exec bot yarn migrate
