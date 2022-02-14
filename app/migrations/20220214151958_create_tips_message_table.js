657656efd8ff045ffb951e4449cca262bd204691
hanshino <hanshino@github>
2022-02-14 15:35:31 +0000

    提示小幫助推播

---
+++ app/migrations/20220214151958_create_tips_message_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("tips_message", table => {
+    table.increments("id").primary();
+    table.string("message").notNullable().comment("訊息內容");
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("tips_message");
+};
