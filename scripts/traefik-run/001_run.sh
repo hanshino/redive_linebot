270780f221aba3bac5f53d9275706fdcead2d507
hanshino <hanshino@github>
2022-04-30 18:43:35 +0000

    腳本調整

--- scripts/traefik-run/001_run.sh
+++ scripts/traefik-run/001_run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+"${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml pull
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml build
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml up -d
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml exec -T bot yarn migrate
