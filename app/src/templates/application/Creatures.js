440396126504fd4a5992e85ca3a3ef90ea11a1c3
hanshino <hanshino@github>
2022-06-11 01:45:57 +0000

    新增餵食功能

--- app/src/templates/application/Creatures.js
+++ app/src/templates/application/Creatures.js
@@ -1,5 +1,6 @@
 const i18n = require("../../util/i18n");
 const config = require("config");
+const { isEmpty } = require("lodash");
 
 /**
  * 產出創角色用的 bubble
@@ -155,12 +156,13 @@ exports.generateCreateBubble = ({
 /**
  * 產出角色主畫面用的 bubble
  */
-exports.generateCreatureMainBubble = ({
+exports.generateMainBubble = ({
   image_url: image,
   nickname,
   satiety,
   favorability,
   stamina,
+  exp: experience,
 }) => {
   return {
     type: "bubble",
@@ -231,6 +233,43 @@ exports.generateCreatureMainBubble = ({
                 }),
               ],
             },
+            {
+              type: "box",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "text",
+                  text: i18n.__("template.experience"),
+                  size: "sm",
+                  color: "#808080",
+                },
+                {
+                  type: "box",
+                  layout: "horizontal",
+                  contents: [
+                    {
+                      type: "box",
+                      layout: "vertical",
+                      contents: [],
+                      backgroundColor: "#AAFFAA",
+                      width: "5%", // 經驗值 %數 暫時寫死
+                    },
+                  ],
+                  spacing: "sm",
+                  height: "5px",
+                  backgroundColor: "#808080",
+                  cornerRadius: "md",
+                  margin: "sm",
+                },
+                {
+                  type: "text",
+                  text: `${experience}`,
+                  align: "end",
+                  size: "xxs",
+                  color: "#4588FF",
+                },
+              ],
+            },
           ],
           spacing: "xs",
         },
@@ -249,4 +288,116 @@ function generateIconBaseline({ url, number }) {
     })),
     spacing: "sm",
   };
+}
+
+exports.generateFoodBubble = foods => ({
+  type: "bubble",
+  body: {
+    type: "box",
+    layout: "vertical",
+    contents: [
+      {
+        type: "text",
+        text: i18n.__("template.okashiya"),
+      },
+      ...foods,
+    ],
+    spacing: "xxl",
+  },
+});
+
+/**
+ * 產出單一食物的 Box
+ * @param {Object} param0
+ * @param {String} param0.name
+ * @param {String} param0.description
+ * @param {String} param0.image_url
+ * @param {Array} param0.effects
+ * @returns {Object}
+ */
+exports.generateFoodItemBox = ({ id, name, image_url, description, effects }) => {
+  return {
+    type: "box",
+    layout: "horizontal",
+    contents: [
+      {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "image",
+            url: `${image_url}`,
+          },
+          {
+            type: "box",
+            layout: "vertical",
+            contents: [
+              {
+                type: "text",
+                text: i18n.__("template.use"),
+                size: "xxs",
+                align: "center",
+                color: "#F0F0F0",
+              },
+            ],
+            cornerRadius: "md",
+            paddingAll: "1px",
+            backgroundColor: "#943872",
+            margin: "sm",
+          },
+        ],
+        action: {
+          type: "postback",
+          data: JSON.stringify({
+            action: "useFood",
+            id,
+          }),
+          displayText: `${i18n.__("template.use")} ${name}`,
+        },
+      },
+      {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "text",
+            text: `${name}`,
+            size: "sm",
+            weight: "bold",
+          },
+          {
+            type: "text",
+            text: `${description}`,
+            size: "xs",
+          },
+          {
+            type: "text",
+            size: "xxs",
+            color: "#808080",
+            contents: generateFoodItemEffects(effects),
+          },
+        ],
+        flex: 5,
+      },
+    ],
+    spacing: "md",
+  };
+};
+
+/**
+ * 產出食物的效果文字
+ * @param {Array<{type: String, value: Number}>} effects
+ * @returns {Array<Object>}
+ */
+function generateFoodItemEffects(effects = []) {
+  if (isEmpty(effects)) {
+    return [{ type: "span", text: "-" }];
+  }
+
+  return effects.map(effect => {
+    return {
+      type: "span",
+      text: `+${effect.value} ${i18n.__(`template.${effect.type}`)}`,
+    };
+  });
 }
