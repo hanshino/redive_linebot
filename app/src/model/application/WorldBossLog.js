465c78157a398cda544599ca22da3ba714502978
hanshino <c0952199517@gmail.com>
2023-02-08 14:32:05 +0800

    面板調整

--- app/src/model/application/WorldBossLog.js
+++ app/src/model/application/WorldBossLog.js
@@ -126,15 +126,15 @@ exports.getTotalDamageByEventId = async event_id => {
 };
 
 /**
- * 取得某個活動的前十名 (排名)
+ * 取得某個活動排名
  */
-exports.getTopTen = async event_id => {
+exports.getTopRank = async ({ eventId, limit }) => {
   // SELECT sum(`damage`) as `total_damage`, `User`.`platformId` FROM `world_boss_event_log` JOIN `User` ON `world_boss_event_log`.`user_id` = `User`.`No` WHERE `world_boss_event_id` = 1 GROUP BY `user_id`
   return await mysql(TABLE)
     .select(mysql.raw("sum(`damage`) as `total_damage`, `User`.`platformId` as `userId`"))
     .join("User", "world_boss_event_log.user_id", "User.No")
-    .where("world_boss_event_id", event_id)
+    .where("world_boss_event_id", eventId)
     .groupBy("user_id")
     .orderBy("total_damage", "desc")
-    .limit(10);
+    .limit(limit);
 };
