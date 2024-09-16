889dea716261b2c299a98087b309a7f85a62586a
hanshino <c0952199517@gmail.com>
2024-09-16 12:03:10 +0800

    refactor: 移除錯誤處理中的Sentry初始化和捕獲例外

--- app/_error.js
+++ app/_error.js
@@ -1,13 +1,3 @@
-const Sentry = require("@sentry/node");
-// or use es6 import statements
-// import * as Sentry from '@sentry/node';
-
-Sentry.init({ dsn: process.env.SENTRY_DSN });
-
 module.exports = async function HandleError(context, props) {
   console.error(props.error.stack);
-
-  if (process.env.NODE_ENV === "production") {
-    Sentry.captureException(props.error);
-  }
 };
