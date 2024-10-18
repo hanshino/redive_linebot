b45bc81ed674d56be893f27e36f2117961bbd12d
hanshino <c0952199517@gmail.com>
2024-10-18 18:00:32 +0800

    chore: 調整 cicd 機制

--- scripts/traefik-run/001_run.sh
+++ scripts/traefik-run/001_run.sh
@@ -3,4 +3,5 @@
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml pull
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml build
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml up -d
+"${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml restart bot
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml exec -T bot yarn migrate
