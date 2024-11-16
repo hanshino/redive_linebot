d197a4c29a0557bd4e4a8348ca5dae054cecb741
hanshino <c0952199517@gmail.com>
2024-11-16 16:38:41 +0800

    feat: 修改歐洲抽時間

--- app/src/controller/princess/gacha.js
+++ app/src/controller/princess/gacha.js
@@ -184,7 +184,7 @@ async function gacha(context, { match, pickup, ensure = false, europe = false })
   const now = moment();
   const month = now.month() + 1;
   const date = now.date();
-  const isEventTime = month === 10 && date >= 6 && date <= 11;
+  const isEventTime = month === 11 && date >= 17 && date <= 22;
 
   // 只有 12/31~1/1 這兩天才會開放歐洲轉蛋池
   if (europe && !isEventTime) {
