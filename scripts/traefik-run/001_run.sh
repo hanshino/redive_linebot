f2a75c423146822ae7cd6f31d1c4c46d8f716985
hanshino <c0952199517@gmail.com>
2024-12-16 16:53:58 +0800

    feat: 在啟動過程中新增等待 MySQL 啟動的延遲

--- scripts/traefik-run/001_run.sh
+++ scripts/traefik-run/001_run.sh
@@ -4,4 +4,6 @@
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml build
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml up -d
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml restart bot
+# Wait for the MySQL to start
+sleep 10
 "${COMPOSE_CMD[@]}" -f docker-compose.traefik.yml exec -T bot yarn migrate
