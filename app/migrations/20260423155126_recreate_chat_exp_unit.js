c2b5f5543f526cf5cde8549b243e98f1343cbd38
hanshino <c0952199517@gmail.com>
2026-04-23 15:51:47 +0000

    feat(chat-level): migration — recreate chat_exp_unit with new square-law curve schema

---
+++ app/migrations/20260423155126_recreate_chat_exp_unit.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+const { Knex } = require("knex");
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async function (knex) {
+  await knex.schema.dropTableIfExists("chat_exp_unit");
+  return knex.schema.createTable("chat_exp_unit", table => {
+    table.smallint("unit_level").unsigned().notNullable().primary().comment("0-100, 新曲線平方律");
+    table
+      .integer("total_exp")
+      .unsigned()
+      .notNullable()
+      .comment("累計 XP: round(2.7 * unit_level^2)");
+  });
+};
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = async function (knex) {
+  return knex.schema.dropTableIfExists("chat_exp_unit");
+};
